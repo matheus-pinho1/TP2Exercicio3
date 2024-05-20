609356fcb22dcdb4d1aa10b7954adaaa4a6bc92b
mathe <mathe@MATHEUS>
2024-05-20 20:43:43 -0300

    Adicionar arquivos de projeto.

---
+++ TP2Exercicio3/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace TP2Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            bool ehPrimo = EhNumeroPrimo(numero);
+
+            if (ehPrimo)
+            {
+                Console.WriteLine($"{numero} é um número primo.");
+            }
+            else
+            {
+                Console.WriteLine($"{numero} não é um número primo.");
+            }
+
+            static bool EhNumeroPrimo(int num)
+            {
+                
+                if (num <= 1)
+                {
+                    return false;
+                }
+
+                
+                for (int i = 2; i <= Math.Sqrt(num); i++)
+                {
+                    if (num % i == 0)
+                    {
+                        return false; 
+                    }
+                }
+
+               
+                return true;
+            }
+        }
+    }
+}
+
